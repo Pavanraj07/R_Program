f1e1c0124a7bbc5d5a3c5deb03c6928628ecc748
Pavanraj07 <pavanrajcsd152@gmail.com>
2025-01-10 21:05:34 +0530

    first commit

---
+++ prg1.r
@@ -0,0 +1,26 @@
+# Assign different type of values to variables and display the type of variable. Assign different
+#  types such as Double, Integer, Logical, Complex and Character and understand the difference
+#  between each data type
+
+double_var<-3.14
+class(double_var)
+double_var
+
+integer_var<-42
+class(integer_var)
+integer_var
+
+logical_var<-TRUE
+class(logical_var)
+logical_var
+
+complex_var<-1+2i
+class(complex_var)
+complex_var
+
+character_var<-"Hello, World!"
+class(character_var)
+character_var
+
+x<-10 
+y<-5
