22782097e63d29a17d3a06dd9ca9c7c196b8ebde
Pavanraj07 <pavanrajcsd152@gmail.com>
2025-01-11 15:54:58 +0530

    commit

---
+++ BindingFunction.r
@@ -0,0 +1,10 @@
+#5. Demonstrate the Creation of Matrices from Vectors using Binding Function
+
+ vector1<-c(1,2,3)
+ vector2<-c(4,5,6)
+
+ matrix_rowbind<-rbind(vector1,vector2)
+ matrix_colbind<-cbind(vector1,vector2)
+
+ matrix_rowbind
+ matrix_colbind
