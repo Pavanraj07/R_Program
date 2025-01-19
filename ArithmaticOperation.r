4e8bf91f74a1660322ba2ac4d560815496ee9e89
Pavanraj07 <pavanrajcsd152@gmail.com>
2025-01-19 20:38:10 +0530

    commit

---
+++ ArithmaticOperation.r
@@ -0,0 +1,18 @@
+ #Demonstrate Arithmetic Operation with simple examples.
+ x=4
+ y=2
+ #Arithmetic Operations
+ addition_result<-x+y
+ addition_result
+
+ subtraction_result<-x-y
+ subtraction_result
+
+ multiplication_result<-x*y
+ multiplication_result
+
+ division_result<-x/y
+ division_result
+
+ modulus_result<-x%%y
+ modulus_result
