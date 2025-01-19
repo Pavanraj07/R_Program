037124839aeea25eb35db98d47aafbd04c8d781b
Pavanraj07 <pavanrajcsd152@gmail.com>
2025-01-19 20:39:16 +0530

    commit

---
+++ LogicalOperation.r
@@ -0,0 +1,18 @@
+#Logical Operations
+is_equal<-x==y
+is_equal
+
+is_grater_than<-x>y
+is_grater_than
+
+is_not_equal <-x!=y
+is_not_equal
+
+logical_and<-TRUE&FALSE
+logical_and
+
+logical_or<-TRUE|FALSE
+logical_or
+
+logical_not<-!TRUE
+logical_not
