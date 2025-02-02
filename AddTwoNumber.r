118d45b4ae672701361309c97c6c458f1931327a
Pavanraj <pavanrajcsd152@gmail.com>
2025-02-02 18:23:15 +0530

    commit

---
+++ AddTwoNumber.r
@@ -0,0 +1,7 @@
+# Simple R Program: Adding Two Numbers
+num1 <- as.numeric(readline("Enter first number: "))
+num2 <- as.numeric(readline("Enter second number: "))
+
+sum <- num1 + num2
+
+cat("The sum of", num1, "and", num2, "is:", sum, "\n")
